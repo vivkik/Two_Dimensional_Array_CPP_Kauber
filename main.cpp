56757a08ba693685c94b367e09a0d22376c50e85
William Kauber <102266432+vivkik@users.noreply.github.com>
2022-04-04 21:14:58 +0000

    Final cleanup and comments added

--- main.cpp
+++ main.cpp
@@ -33,7 +33,8 @@ int main()
     int fairbanks[7] = {21, 29, 42, 39, 24, 23, 27};
 
   
-    
+ cout << "Now printing seven day forecast for Fairbank and Osaka:\n"<<endl;
+  
     for (int c = 0; c < CITY; c++) // for loop inputting both city arrays into two dimensional array based on city value.
       {
         for(int w = 0; w < WEEK; w++)
@@ -46,23 +47,25 @@ int main()
               temperature[c][w] = osaka[w];     
             } 
       }
-    for (int c = 0; c < CITY; c++) // for printing 
+    for (int c = 0; c < CITY; c++) // for loop for printing multi-dimensional arrays to screen.  
       {
         for ( int w = 0; w < WEEK; w ++)
           {
             if (c == 0)
             {
-              cout << "Fairbanks, Day" << w +1 << ": "<<temperature[c][w]<< endl;
+              cout << "Fairbanks, Day " << w +1 << ": "<<temperature[c][w]<< endl;
             }
             else if (c == 1)
             {
-              cout << "Osaka, Day" << w + 1 << ": " <<temperature[c][w] << endl;
+              cout << "Osaka, Day " << w + 1 << ": " <<temperature[c][w] << endl;
             }
           }
       }
    
 
-    cout << "\nNow its your turn! Enter all temperature for a week of first city and then second city. \n";
+    cout << "\nNow its your turn!\n";
+    cout << "Enter the temperature for each day of the week"<<endl;
+    cout << "for your first city and then your second city. \n";
  
     // Inserting the values into the temperature array
     //note for every dimension of the array you need a for loop 2 dimensions = 2 for loops
@@ -75,7 +78,7 @@ int main()
         }
     }
  
-    cout << "\n\nDisplaying Values:\n";
+    cout << "\n\nDisplaying Temperatures:\n";
  
     // Accessing the values from the temperature array
     for (int i = 0; i < CITY; ++i)
